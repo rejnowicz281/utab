431616838dc69a7e124b0af9d0f312bba1d77c1d
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2026-01-03 13:54:46 +0100

    fix sticky

--- src/components/organisms/tanstack-table/providers/tanstack-table-provider/tanstack-table-provider.tsx
+++ src/components/organisms/tanstack-table/providers/tanstack-table-provider/tanstack-table-provider.tsx
@@ -9,8 +9,13 @@ export interface ITanstackTableProps<T> {
     stickyRight?: boolean;
 }
 
-export function TanstackTableProvider<T>({ children, ...props }: PropsWithChildren<ITanstackTableProps<T>>) {
-    const value = useTanstackTableProvider(props);
+export function TanstackTableProvider<T>({
+    children,
+    stickyLeft = true,
+    stickyRight = true,
+    ...props
+}: PropsWithChildren<ITanstackTableProps<T>>) {
+    const value = useTanstackTableProvider({ ...props, stickyLeft, stickyRight });
 
     return <TanstackTableContext.Provider value={value}>{children}</TanstackTableContext.Provider>;
 }
